e12c98cf73df72c52b301dbfed57a586a13dc0bb
EtienneV <formation.etienne.re@gmail.com>
2022-01-11 09:51:54 +0400

    Random § Statefull

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -9,15 +10,15 @@ void main() {
           title: Text('Dédé'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: DicePage2(),
       ),
     ),
   );
 }
 
 class DicePage extends StatelessWidget {
-  int leftDiceNumber = 5;
-  int rightDiceNumber = 6;
+  int leftDiceNumber = Random().nextInt(6) + 1;
+  int rightDiceNumber = Random().nextInt(6) + 1;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -28,6 +29,7 @@ class DicePage extends StatelessWidget {
             padding: const EdgeInsets.all(16.0),
             child: TextButton(
                 onPressed: () {
+                  leftDiceNumber = 6;
                   print("Vous avez appuyé sur l'image de gauche");
                 },
                 child: Image.asset("images/dice$leftDiceNumber.png")),
@@ -37,6 +39,53 @@ class DicePage extends StatelessWidget {
             padding: const EdgeInsets.all(16.0),
             child: TextButton(
                 onPressed: () {
+                  print("Vous avez appuyé sur l'image de droite");
+                },
+                child: Image.asset("images/dice$rightDiceNumber.png")),
+          )),
+        ],
+      ),
+    );
+  }
+}
+
+class DicePage2 extends StatefulWidget {
+  const DicePage2({Key? key}) : super(key: key);
+
+  @override
+  _DicePage2State createState() => _DicePage2State();
+}
+
+class _DicePage2State extends State<DicePage2> {
+  int leftDiceNumber = Random().nextInt(6) + 1;
+  int rightDiceNumber = Random().nextInt(6) + 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    leftDiceNumber = Random().nextInt(6) + 1;
+                    rightDiceNumber = Random().nextInt(6) + 1;
+                  });
+                  print("Vous avez appuyé sur l'image de gauche");
+                },
+                child: Image.asset("images/dice$leftDiceNumber.png")),
+          )),
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    leftDiceNumber = Random().nextInt(6) + 1;
+                    rightDiceNumber = Random().nextInt(6) + 1;
+                  });
                   print("Vous avez appuyé sur l'image de droite");
                 },
                 child: Image.asset("images/dice$rightDiceNumber.png")),
